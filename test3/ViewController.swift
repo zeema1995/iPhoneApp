03c32697072efeb33728d775056977d5233765c1
中島 祐也 <zeema_1995@yahoo.co.jp>
2020-05-10 22:57:54 +0900

    あああ

--- test3/ViewController.swift
+++ test3/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .blue
-        titleButton.title = "タイトルです"
+        titleButton.title = "タイトルですよ"
         // Do any additional setup after loading the view.
     }
     //test1
